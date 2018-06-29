2869364d3ef9c364a07163299b85989b160c04c5
StephenMcGuckinDAT <stephendmcguckin@gmail.com>
2018-06-28 23:23:29 -0700

    removed useless test

--- spec/hasher_spec.rb
+++ spec/hasher_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Hasher do
   it 'has a version number' do
     expect(Hasher::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
